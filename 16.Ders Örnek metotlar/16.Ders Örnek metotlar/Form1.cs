3dc8b3b59951d91f8ac5fe08d44e29c10e14a0bb
İlkay Acar <147315653+ilkaycr@users.noreply.github.com>
2024-03-01 14:11:01 +0300

    Add files via upload

---
+++ 16.Ders Örnek metotlar/16.Ders Örnek metotlar/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16.Ders_Örnek_metotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            if (radioButton1.Checked)
+                label3.Text = topla(a, b).ToString();
+            else if (radioButton2.Checked)
+                label3.Text = cikar(a, b).ToString();
+            else if (radioButton3.Checked)
+                label3.Text = carp(a, b).ToString();
+            else if (radioButton4.Checked)
+                if (b == 0)
+                    label3.Text = "Sıfıra bölme olmaz";
+                else
+                    label3.Text = bol(a, b).ToString();
+
+        }
+        private double topla(double a, double b)
+        {
+            return a + b;
+        }
+        private double cikar(double a, double b)
+        {
+            return a - b;
+        }
+        private double carp(double a, double b)
+        {
+            return a * b;
+        }
+        private double bol(double a, double b)
+        {
+            return a / b;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ 18.Ders Aşırı yüklenmiş metotlar/18.Ders Aşırı yüklenmiş metotlar/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _18.Ders_Aşırı_yüklenmiş_metotlar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // aşırı yüklenmiş metotlar
+            // aynı isimli birden fazla metot tanımladığımız zaman bu metot aşırı yüklenmiş metot oluyor
+            label1.Text = topla(1, 2).ToString();
+        }
+        int topla(int a,int b)
+        {
+            return a + b;
+        }
+
+        double topla(double a, int b)
+        {
+            return a + b;
+        }
+
+        int topla(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+    }
+}
